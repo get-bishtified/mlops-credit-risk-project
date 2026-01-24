250e55226ba54bc834dfeef56bd42dd2d0881cdd
Shankaz <shankazcloud@gmail.com>
2026-01-24 16:49:38 +0530

    updated

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,16 @@
 output "region" {
   value = var.region
 }
+
+output "raw_bucket" {
+  value = aws_s3_bucket.raw.bucket
+}
+
+output "model_bucket" {
+  value = aws_s3_bucket.models.bucket
+}
+
+output "train_repo_url" {
+  value = aws_ecr_repository.train.repository_url
+}
+
